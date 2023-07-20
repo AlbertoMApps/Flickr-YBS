ece3666892155d9904685000047326bf463d8160
Alberto Medrano <albersna@hotmail.com>
2023-07-20 10:46:46 +0100

    Checking nodes for the UI test

--- app/src/androidTest/java/com/alberto/flickrtest/FlickrAlbumTest.kt
+++ app/src/androidTest/java/com/alberto/flickrtest/FlickrAlbumTest.kt
@@ -1,8 +1,12 @@
 package com.alberto.flickrtest
 
 import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToIndex
+import androidx.compose.ui.test.performScrollToNode
 import androidx.compose.ui.test.performTextInput
 import com.alberto.flickrtest.ui.screens.MainActivity
 import org.junit.Rule
@@ -15,14 +19,21 @@ class FlickrAlbumTest {
 
     @Test
     fun testTextField() {
-        rule.onNodeWithTag(
-            "text_field_test_tag"
-        ).performTextInput("Flickr Image")
-        rule.onNodeWithTag("text_field_test_tag").assertTextContains("Flickr Image")
+        rule.apply {
+            onNodeWithTag(
+                "text_field_test_tag"
+            ).performTextInput("Flickr Image")
+            onNodeWithTag("text_field_test_tag").assertTextContains("Flickr Image")
+        }
     }
 
     @Test
     fun testLazyColumnView() {
         rule.onNodeWithTag("list_test_tag").assertExists()
+            .performScrollToIndex(0)
+            .performScrollToIndex(1)
+            .performScrollToIndex(2)
+            .performScrollToIndex(3)
+            .performClick()
     }
 }
